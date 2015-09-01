de6babd4950c5cc8ff88014696354577874611cc
garbinlin <garbinlin@gmail.com>
2015-09-01 18:22:21 +0800

    挂载第三方资源在自己服务器上

--- Public/include/jquery_mobile_resources.php
+++ Public/include/jquery_mobile_resources.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
-<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+<link rel="stylesheet" href="/Public/resources/jquery/mobile/css/jquery.mobile-1.3.2.min.css">
+<script src="/Public/resources/jquery/jquery-1.8.3.min.js"></script>
+<script src="/Public/resources/jquery/mobile/js/jquery.mobile-1.3.2.min.js"></script>
