9f84698a7943e5919f282b811401d371bef2902f
garbinlin <garbinlin@gmail.com>
2015-09-01 19:41:26 +0800

    jquery mobile images

--- Public/include/jquery_mobile_resources.php
+++ Public/include/jquery_mobile_resources.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width" />
 <link rel="stylesheet" href="/Public/resources/jquery/mobile/css/jquery.mobile-1.3.2.min.css">
 <script src="/Public/resources/jquery/jquery-1.8.3.min.js"></script>
 <script src="/Public/resources/jquery/mobile/js/jquery.mobile-1.3.2.min.js"></script>
