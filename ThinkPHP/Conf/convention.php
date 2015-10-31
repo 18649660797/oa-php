7ee13db9c8720a1bc2bc72df6ff2d486a68c262a
linjb <linjb@ecmaster.cn>
2015-10-31 16:17:49 +0800

    结构整理

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -58,7 +58,7 @@ return  array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'edy_tmp',          // 数据库名
+    'DB_NAME'               =>  'edy',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
